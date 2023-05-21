f88fbdada97263c24b2804e969429970c81c4a72
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-05-21 23:16:07 +0600

    updated button

--- src/Pages/AddAToy/AddAToy.jsx
+++ src/Pages/AddAToy/AddAToy.jsx
@@ -145,6 +145,12 @@ const AddAToy = () => {
         >
           Add Toy
         </button>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Delete
+        </button>
       </form>
     </div>
   );
