8b0524b302fe14cb5b8e0a60a937a77c15ffa7ac
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-05-21 20:17:11 +0600

    added featured section

--- src/Home/FeaturedToys/FeaturedToys.jsx
+++ src/Home/FeaturedToys/FeaturedToys.jsx
@@ -1,19 +1,25 @@
+import './FeaturedToys.css'; // Import the CSS file for the component
 
 const FeaturedToys = () => {
-  const featuredToys = [
+  const featuredToys =  [
     {
       id: 1,
       name: "Mickey Mouse Plush Toy",
-      image: "mickey-plush.jpg",
+      image: "https://th.bing.com/th?id=OIP.BG5AElXVF3IfQ5c00Xmn0gHaHa&w=249&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
       description: "Cuddle up with this adorable Mickey Mouse plush toy and bring the magic of Disney into your home.",
     },
     {
       id: 2,
       name: "Princess Castle Playset",
-      image: "princess-castle.jpg",
-      description: "Let your child's imagination soar with this enchanting princess castle playset, complete with royal accessories.",
+      image: "https://th.bing.com/th?id=OIP.4PPEItEYvWMSWsbyupzdwQHaJQ&w=223&h=279&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      description: "Let your child's imagination soar with this enchanting princess castle playset, complete with royal accessories."
     },
-    // Add more featured toys
+    {
+      id: 3,
+      name: "Castle Playset",
+      image: "https://th.bing.com/th?id=OIP.pB49t5zHhsRPOctUxLRm7AHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
+      description: "Let your child's imagination soar with this enchanting princess castle playset, complete with royal accessories."
+    }
   ];
 
   return (
